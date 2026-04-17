ea34fe17be217703c8de42de7b82b87bf01bd9d0
Marcin Bratek <github@bratekmarcin.eu>
2026-04-17 12:31:33 +0200

    added empty iOS app project

---
+++ HabitTrackerApp/HabitTrackerApp/HabitTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerAppApp.swift
+//  HabitTrackerApp
+//
+//  Created by Marcin Bratek on 17/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitTrackerApp/HabitTrackerAppTests/HabitTrackerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerAppTests.swift
+//  HabitTrackerAppTests
+//
+//  Created by Marcin Bratek on 17/04/2026.
+//
+
+import Testing
+@testable import HabitTrackerApp
+
+struct HabitTrackerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
